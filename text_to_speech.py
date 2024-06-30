2ce0cc1ac6c0f5a49a09b0137b9c3f1c31cbdaad
sophia walton <srw9rx@virginia.edu>
2024-06-30 13:34:25 -0400

    replace_words function to expand reddit acronyms

--- text_to_speech.py
+++ text_to_speech.py
@@ -9,7 +9,10 @@ def replace_words(text:str, vocab_path:str) -> str:
     :param vocab_path: the path to a json document of vocab being replaced
     :return: the string with the listed vocabulary words replaced'''
     assert(vocab_path[-4:]=='json')
-    mappings = json.loads(vocab_path) # read in the json document
+    with open(vocab_path, 'r') as f:
+        mappings = json.load(f) # read in the json document
+    for mapping in mappings:
+        text = text.replace(mapping['term'], mapping['replacement'])
     return text
     
 
@@ -20,9 +23,12 @@ def text_to_speech(text:str, path:str):
     :return: the file path that will be used to get the mp3 to overlay on video
     '''
     tts = gTTS(text)
-    time = datetime.datetime.now().strftime('%d-%m-%Y%H:%M:%S')
+    time = datetime.datetime.now().strftime('%d-%m-%YT%H:%M:%S')
     path = f'{path}/story_{time}.mp3'
     tts.save(savefile=path)
     return path
 
-text_to_speech("AITA for slapping a teenager? \n I (32f) was at a water park this last weekend with my husband (32m) and my daughter. We were in one of the pools practicing swimming and keeping to our self. There was a group of teen boys there and while I was working with my daughter on swimming one of them came up behind me and I felt a tug on the strings of my top untying it. I spun around saw this 15 to 17 yo with a smirk and slapped him. \nThis quickly caused a scene. The park staff got involved as well the boys parents who were livid at me. My husband and another lady saw it happen and confirmed that he really did grab my top. There was also camera around the pool that kind of show it, wasn't the best angle. The boys parents threaten assault charges and I threaten sexual assault charges if they decided to go that way. Eventually we were both asked to leave and haven't heard anything since. My husband though still thinks I over reacted a bit which I don't. AITA?", "stories/speech")
+text = "AITA for slapping a teenager? \n I (32f) was at a water park this last weekend with my husband (32m) and my daughter. We were in one of the pools practicing swimming and keeping to our self. There was a group of teen boys there and while I was working with my daughter on swimming one of them came up behind me and I felt a tug on the strings of my top untying it. I spun around saw this 15 to 17 yo with a smirk and slapped him. \nThis quickly caused a scene. The park staff got involved as well the boys parents who were livid at me. My husband and another lady saw it happen and confirmed that he really did grab my top. There was also camera around the pool that kind of show it, wasn't the best angle. The boys parents threaten assault charges and I threaten sexual assault charges if they decided to go that way. Eventually we were both asked to leave and haven't heard anything since. My husband though still thinks I over reacted a bit which I don't. AITA?"
+text = replace_words(text, 'reddit_vocab.json')
+print(text)
+text_to_speech(text, "stories/speech")
